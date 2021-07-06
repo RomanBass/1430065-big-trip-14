6a0f74c62fff7c1aa9ae628a200d0eab41b17b4f
Roman Katrovsky <katrovsky@gmail.com>
2021-07-06 15:26:21 +0300

    выполняет по коммиту 3.1.1 УП

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import {createEditFormTemplate} from './view/edit-form.js';
 import {createTripPointTemplate} from './view/trip-point.js';
 import {generateTripPoint} from './mock/trip-point.js';
 import { getRouteDates, getRoutePrice, getRouteName } from './utils/route.js';
+import { renderTemplate } from './utils/render.js';
 
 const TRIP_POINT_COUNT = 20;
 const tripPoints = new Array(TRIP_POINT_COUNT).fill().map(generateTripPoint); // массив точек маршрута
@@ -15,29 +16,25 @@ tripPoints.sort((a, b) => { // сортировка по dateFrom
   return (a.dateFrom - b.dateFrom);
 });
 
-const render = (container, template, place) => { // отрисовывает элементы в контейнеры с учётом расположения
-  container.insertAdjacentHTML(place, template);
-};
-
 const siteHeaderElement = document.querySelector('.page-header'); // крупный блок
 const menuElement = siteHeaderElement.querySelector('.trip-controls__navigation'); // контейнеры...
 const tripElement = siteHeaderElement.querySelector('.trip-main');
 const filtersElement = siteHeaderElement.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-render(menuElement, createSiteMenuTemplate(), 'beforeend'); // отрисовки..
-render(tripElement, createInfoAndPriceTemplate(getRoutePrice(tripPoints), getRouteDates(tripPoints), getRouteName(tripPoints)), 'afterbegin');
-render(filtersElement, createFiltersTemplate(), 'beforeend');
-render(tripEventsElement, createSortingTemplate(), 'beforeend');
-render(tripEventsElement, createEventsList(), 'beforeend');
+renderTemplate(menuElement, createSiteMenuTemplate(), 'beforeend'); // отрисовки..
+renderTemplate(tripElement, createInfoAndPriceTemplate(getRoutePrice(tripPoints), getRouteDates(tripPoints), getRouteName(tripPoints)), 'afterbegin');
+renderTemplate(filtersElement, createFiltersTemplate(), 'beforeend');
+renderTemplate(tripEventsElement, createSortingTemplate(), 'beforeend');
+renderTemplate(tripEventsElement, createEventsList(), 'beforeend');
 
 const tripEventsList = tripEventsElement.querySelector('.trip-events__list'); // переменная - контейнер для списка точек
 
-render(tripEventsList, createEditFormTemplate(), 'beforeend'); // отрисовка формы создания точки
-render(tripEventsList, createEditFormTemplate(tripPoints[0]), 'beforeend'); // отрисовка формы редактирования точки
+renderTemplate(tripEventsList, createEditFormTemplate(), 'beforeend'); // отрисовка формы создания точки
+renderTemplate(tripEventsList, createEditFormTemplate(tripPoints[0]), 'beforeend'); // отрисовка формы редактирования точки
 
 for (let i = 1; i < TRIP_POINT_COUNT; i++) { // отрисовка точек маршрута
-  render(tripEventsList, createTripPointTemplate(tripPoints[i]), 'beforeend');
+  renderTemplate(tripEventsList, createTripPointTemplate(tripPoints[i]), 'beforeend');
 }
 
 export {tripPoints};
--- src/utils/render.js
+++ src/utils/render.js
@@ -1,3 +1,29 @@
+export const RenderPosition = {
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
 export const getCitiesUniqueNames = (tripPoints) => { // выдаёт отсортированный массив уникальных названий городов из массива точек маршрута
   let citiesNames = new Set();
   tripPoints.forEach((point) => {
