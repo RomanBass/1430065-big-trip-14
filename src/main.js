5990e9788781d0926ffa1a54d82932efaa21e044
Roman Katrovsky <katrovsky@gmail.com>
2021-07-07 15:27:53 +0300

    выполняет действия по коммиту 3.1.6. УП

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import SortingView from './view/sorting.js';
 import EventListView from './view/events-list.js';
 import {generateTripPoint} from './mock/trip-point.js';
 import { getRouteDates, getRoutePrice, getRouteName } from './utils/route.js';
-import {renderElement, RenderPosition} from './utils/render.js';
+import {render, RenderPosition} from './utils/render.js';
 import TripPointView from './view/trip-point.js';
 import EditFormView from './view/edit-form.js';
 
@@ -22,19 +22,19 @@ const tripElement = siteHeaderElement.querySelector('.trip-main');
 const filtersElement = siteHeaderElement.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-renderElement(menuElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND); // отрисовки компонентов...
-renderElement(tripElement, new InfoAndPriceView(getRoutePrice(tripPoints), getRouteDates(tripPoints), getRouteName(tripPoints)).getElement(), RenderPosition.AFTERBEGIN);
-renderElement(filtersElement, new FilterView().getElement(), RenderPosition.BEFOREEND);
-renderElement(tripEventsElement, new SortingView().getElement(), RenderPosition.BEFOREEND);
-renderElement(tripEventsElement, new EventListView().getElement(), RenderPosition.BEFOREEND);
+render(menuElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND); // отрисовки компонентов...
+render(tripElement, new InfoAndPriceView(getRoutePrice(tripPoints), getRouteDates(tripPoints), getRouteName(tripPoints)).getElement(), RenderPosition.AFTERBEGIN);
+render(filtersElement, new FilterView().getElement(), RenderPosition.BEFOREEND);
+render(tripEventsElement, new SortingView().getElement(), RenderPosition.BEFOREEND);
+render(tripEventsElement, new EventListView().getElement(), RenderPosition.BEFOREEND);
 
 const tripEventsList = tripEventsElement.querySelector('.trip-events__list'); // переменная - контейнер для списка точек
 
-renderElement(tripEventsList, new EditFormView().getElement(), RenderPosition.BEFOREEND); // отрисовка формы создания точки
-renderElement(tripEventsList, new EditFormView(tripPoints[0]).getElement(), RenderPosition.BEFOREEND); // отрисовка формы редактирования точки
+render(tripEventsList, new EditFormView().getElement(), RenderPosition.BEFOREEND); // отрисовка формы создания точки
+render(tripEventsList, new EditFormView(tripPoints[0]).getElement(), RenderPosition.BEFOREEND); // отрисовка формы редактирования точки
 
 for (let i = 1; i < TRIP_POINT_COUNT; i++) { // отрисовка точек маршрута
-  renderElement(tripEventsList, new TripPointView(tripPoints[i]).getElement(), RenderPosition.BEFOREEND);
+  render(tripEventsList, new TripPointView(tripPoints[i]).getElement(), RenderPosition.BEFOREEND);
 }
 
 export {tripPoints};
