d03681e845ccc4da6a4b4dfbdc2a7cc41fdb87be
Roman Katrovsky <katrovsky@gmail.com>
2021-07-16 06:43:29 +0300

    5.1.3

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,11 +1,11 @@
-import SortingView from './view/sorting.js'; // |---> относятся к презентеру маршрута
-import PointView from './view/point';
-import EditFormView from './view/edit-form.js';
-import NoPointView from './view/no-point.js';
-import EventsListView from './view/events-list.js';
-import {generatePoint} from './mock/point.js';
-import { getRouteDates, getRoutePrice, getRouteName } from './utils/route.js';
-import {render, RenderPosition, replace /*remove*/} from './utils/render.js';
+import SortingView from '../view/sorting.js'; // |---> относятся к презентеру маршрута
+import PointView from '../view/point';
+import EditFormView from '../view/edit-form.js';
+import NoPointView from '../view/no-point.js';
+import EventsListView from '../view/events-list.js';
+import {generatePoint} from '../mock/point.js';
+import { getRouteDates, getRoutePrice, getRouteName } from '../utils/route.js';
+import {render, RenderPosition, replace /*remove*/} from '../utils/render.js';
 
 
 export default class Trip {
@@ -21,27 +21,30 @@ export default class Trip {
 
   init(points) {
     this._points = points.slice();
+    this._renderSort();
+    this._renderEventsList();
+
 
   }
 
   _renderSort() {
-
+    render(this._tripContainer, this._sortingComponent, RenderPosition.AFTERBEGIN);
   }
 
-  _renderPoint() {
+  _renderPoint(point) {
 
   }
 
   _renderPoints() {
-
+    this._points.slice().forEach((point) => this._renderPoint(point));
   }
 
   _renderNoPoints() {
-
+    render(this._tripContainer, this._noPointComponent, RenderPosition.BEFOREEND);
   }
 
   _renderEventsList() {
-
+    render(this._tripContainer, this._eventsListComponent, RenderPosition.BEFOREEND);
   }
 
 }
